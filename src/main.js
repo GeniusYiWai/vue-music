f9eff61929a0a2de82d5c4d475b8f37e4cfbff7a
意外 <lpf1152248628@126.com>
2020-10-08 20:20:55 +0800

    添加nprogress

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+// 引入 nprogress 样式文件
+import "nprogress/nprogress.css";
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-
+import NProgress from 'nprogress'
 const routes = [
   // {
   //   path: '/',
   //   component: () => import( '../views/About.vue')
   // }
 ]
+
+
+
+
+//路由跳转添加nprogress进度条
+
+router.beforeEach((to, from, next) => {
+  //nprogress进度条显示
+  NProgress.start()
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  // nprogress进度条隐藏
+  NProgress.done()
+
+})
+
+
+
+
+
 
 const router = new VueRouter({
   routes
